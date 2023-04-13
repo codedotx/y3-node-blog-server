2e3472bfe023d0f00e1e0f302acd00f0b866e6d2
Yasoob Rasheed <yasoob.f.rasheed@gmail.com>
2023-04-12 23:53:19 -0700

    organizing get, post, put, and delete endpoints

--- app.js
+++ app.js
@@ -4,13 +4,43 @@ const express = require('express');
 const app = express();
 const port = 3000 // tells us what port on your computer to listen to
 
+app.use(express.json()) // automatically turn req to json
+
 /* 
-Handler when request is made to the index
-- req is the contents of the request sent to the server
-- res is what you are sending back to the client
+    GET Requests
 */
 app.get('/', (req, res) => {
-    res.send(hello_world());
+    res.send('GET home')
+})
+
+app.get('/posts/', (req, res) => {
+    res.send('GET posts')
+})
+
+app.get('/posts/:postId/', (req, res) => {
+    res.send(`GET post with post id: ${req.params.postId}`)
+})
+
+/* 
+    POST Request(s)
+*/
+app.post('/posts/write-post/', (req, res) => {
+    res.send(`POST write-post with content: ${JSON.stringify(req.body)}`)
+})
+
+/* 
+    PUT Request(s)
+*/
+// Note: this does the same thing as the POST request above
+app.put('/posts/write-post/', (req, res) => {
+    res.send(`PUT write-post with content: ${JSON.stringify(req.body)}`)
+})
+
+/* 
+    DELETE Request(s)
+*/
+app.delete('/posts/:postId/', (req, res) => {
+    res.send(`DELETE post with post id: ${req.params.postId}`)
 })
 
 // start server and listen for incoming requests
